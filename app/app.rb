c031814cbca175fcd0c293f2d5d077abec8522c0
Terry <terry@terrytilley.com>
2016-07-27 10:26:53 +0100

    Added Tag class and setup DataMapper

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,7 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
-require_relative 'models/link'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
@@ -15,8 +15,11 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
-    redirect '/links'
+    link  = Link.new(url: params[:url], title: params[:title])
+    tag   = Tag.first_or_create(name: params[:tags])
+    link.tags << tag
+    link.save
+    redirect to('/links')
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
